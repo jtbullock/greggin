1a248a186b1dbb4bf0ba656f0cb28b67f7d522b6
Josh Bullock <mail@joshb.me>
2023-04-16 09:52:03 -0400

    Add a component and stuff ;

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -19,6 +19,7 @@ type Model = {
 type Msg =
     | Inc
     | Dec
+    | Reset
 
 /// The init function is called to start the message pump with an initial view.
 let init () =
@@ -32,15 +33,29 @@ let update msg model =
     match msg with
     | Inc -> { Count = model.Count + 1 }, Cmd.none
     | Dec -> { Count = model.Count - 1 }, Cmd.none
+    | Reset -> init ()
+
+let card (title:string) (content: ReactElement list) =
+    Html.div [
+        prop.style [ style.width 600; style.paddingTop 20; style.margin (0, length.auto)]
+        prop.children [
+            Bulma.box [ prop.children [ Bulma.subtitle title; yield! content ] ]
+        ]
+    ]
 
 /// The view function knows how to render the UI given a model, as well as to dispatch new messages based on user actions.
 let view (model: Model) dispatch =
     Html.div [
         prop.style [ style.backgroundColor "#eeeeee57"; style.minHeight (length.vh 100) ]
         prop.children [
-            Html.h1 "Hello!"
-            Html.text ( sprintf "Counter: %i" model.Count )
-            Html.button [ prop.onClick (fun _ -> dispatch Inc); prop.text "Inc" ]
-            Html.button [ prop.onClick (fun _ -> dispatch Dec); prop.text "Dec" ]
+            card "Counter Test" [
+                Html.text ( sprintf "Counter: %i" model.Count )
+                Html.div [
+                    Html.button [ prop.onClick (fun _ -> dispatch Inc); prop.text "Inc" ]
+                    Html.button [ prop.onClick (fun _ -> dispatch Dec); prop.text "Dec" ]
+                    Html.button [ prop.onClick (fun _ -> dispatch Reset); prop.text "Reset" ]
+                ]
+            ]
+            card "Thing 2" [ (Html.text "Another thing") ]
         ]
     ]
