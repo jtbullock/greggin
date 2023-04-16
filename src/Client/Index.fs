892b2b2f01988b70068d695734026cf9c3004e01
Josh Bullock <mail@joshb.me>
2023-04-16 11:19:29 -0400

    Add ingredients

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -10,29 +10,34 @@ open Elmish.SweetAlert
 
 open Fable.Remoting.Client
 open Shared
+open Fable.Core
 
 /// The overall data model driving the view.
 type Model = {
     Count: int
+    Ingredients: (float32 * string) list
 }
 
 type Msg =
     | Inc
     | Dec
     | Reset
+    | IngredientAdd of (float32 * string)
 
 /// The init function is called to start the message pump with an initial view.
 let init () =
     {
         Count = 0
+        Ingredients = List.empty
     },
     Cmd.none
 
 /// The update function knows how to update the model given a message.
 let update msg model =
     match msg with
-    | Inc -> { Count = model.Count + 1 }, Cmd.none
-    | Dec -> { Count = model.Count - 1 }, Cmd.none
+    | Inc -> { model with Count = model.Count + 1 }, Cmd.none
+    | Dec -> { model with Count = model.Count - 1 }, Cmd.none
+    | IngredientAdd i -> { model with Ingredients = i :: model.Ingredients }, Cmd.none
     | Reset -> init ()
 
 let card (title:string) (content: ReactElement list) =
@@ -40,6 +45,55 @@ let card (title:string) (content: ReactElement list) =
         prop.style [ style.width 600; style.paddingTop 20; style.margin (0, length.auto)]
         prop.children [
             Bulma.box [ prop.children [ Bulma.subtitle title; yield! content ] ]
+        ]
+    ]
+
+
+let parseFloat s = try Some (float s) with | _ -> None
+
+let parseFloatDefaultZero s =
+    match parseFloat s with
+    | Some f -> f
+    | None -> 0.0
+
+[<ReactComponent>]
+let recipeBuilder() =
+
+    let (ingredients, setIngredients) = React.useState(List.empty<double * string>)
+
+    let (quantity, setQuantity) = React.useState(2.5)
+    let (name, setName) = React.useState("")
+
+    let saveIngredient() =
+        setIngredients ((quantity, name) :: ingredients)
+        setQuantity 0.0
+        setName ""
+
+    Html.div [
+        Html.text "Ingredients:"
+
+        Html.div [
+            yield! List.map (fun (qty, name: string) -> Html.text (sprintf "%f %s" qty name)) ingredients 
+        ]
+
+        Html.div [
+            Html.input [
+                prop.type' "number"
+                //prop.step 0.1
+                prop.style [ style.width 50 ]
+                prop.value quantity
+                //prop.onChange (fun (v: string) -> JS.console.log (parseFloatDefaultZero v))
+                prop.onChange (fun (v: string) -> parseFloatDefaultZero v |> setQuantity)
+            ]
+            Html.input [
+                prop.type' "text"
+                prop.value name
+                prop.onChange setName
+            ]
+            Html.button [
+                prop.text "Add"
+                prop.onClick (fun _ -> saveIngredient())
+            ]
         ]
     ]
 
@@ -56,6 +110,17 @@ let view (model: Model) dispatch =
                     Html.button [ prop.onClick (fun _ -> dispatch Reset); prop.text "Reset" ]
                 ]
             ]
-            card "Thing 2" [ (Html.text "Another thing") ]
+            card "Thing 2" [
+                Html.label [
+                    Html.text "Name "
+                    Html.input [
+                        prop.type' "text"
+                    ]
+                ]
+                recipeBuilder ()
+                Html.div [
+                    Html.button [ prop.text "Save" ]
+                ]
+            ]
         ]
     ]
