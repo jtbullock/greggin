23afcc787c7ccf415ada5414728240f6a13b1125
Josh Bullock <mail@joshb.me>
2023-04-20 09:59:47 -0400

    Move sensitive bits to config

--- src/Server/Api.fs
+++ src/Server/Api.fs
@@ -6,21 +6,22 @@ open Azure.Storage.Blobs
 open System.IO
 open Microsoft.Extensions.Configuration
 
-let toMemoryStream (bytes: byte array) = new MemoryStream(bytes)
+type Config =
+    {
+        StorageConnectionString: string
+        ContainerName: string
+        RecipeFileName: string
+    }
 
-let private getRecipesDict (blobConnString) = async {
-    //let storageConnString = "DefaultEndpointsProtocol=https;AccountName=gregginblob;AccountKey=nDHyJevQqHJADdOv9THZsEoYtaXfqS4zEhOXFaEhNsq8AotboemQmD+aDxKnfOStC+FqZj9vepUN+AStGecfwg==;EndpointSuffix=core.windows.net"
+    static member FromIConfiguration(config: IConfiguration) = {
+        StorageConnectionString = config.["ConnectionStrings:BlobStorage"]
+        ContainerName = config.["Storage:ContainerName"]
+        RecipeFileName = config.["Storage:RecipeFileName"]
+    }
 
-    let container = BlobContainerClient(blobConnString, "recipes")
-    let blockBlob = container.GetBlobClient "recipes.json"
+let private toMemoryStream (bytes: byte array) = new MemoryStream(bytes)
 
-    // Fancier way to do this: https://www.planetgeek.ch/2021/04/22/our-journey-to-f-making-async-understand-tasks/
-    let! downloadResult = blockBlob.DownloadAsync() |> Async.AwaitTask
-
-    return JsonSerializer.Deserialize<Map<string, Ingredient list>> downloadResult.Value.Content
-}
-
-let recipeMapToResponseArray (recipes: Map<string, Ingredient list>) =
+let private recipeMapToResponseArray (recipes: Map<string, Ingredient list>) =
     recipes
     |> Map.toArray
     |> Array.map (fun r -> {
@@ -28,43 +29,55 @@ let recipeMapToResponseArray (recipes: Map<string, Ingredient list>) =
         Ingredients = (snd r)
     })
 
-let getRecipes storageConnString = async {
-    let! recipes = getRecipesDict storageConnString
-    return recipeMapToResponseArray recipes
+let private getBlob (config: Config) =
+    let container =
+        BlobContainerClient(config.StorageConnectionString, config.ContainerName)
+
+    container.GetBlobClient "recipes.json"
+
+let private getRecipesDict (blob: BlobClient) = async {
+    // Fancier way to do this: https://www.planetgeek.ch/2021/04/22/our-journey-to-f-making-async-understand-tasks/
+    let! downloadResult = blob.DownloadAsync() |> Async.AwaitTask
+
+    return JsonSerializer.Deserialize<Map<string, Ingredient list>> downloadResult.Value.Content
 }
 
-let uploadRecipes storageConnString recipes =
-    let container = BlobContainerClient(storageConnString, "recipes")
-    let blockBlob = container.GetBlobClient "recipes.json"
-
+let private uploadRecipes (blob: BlobClient) recipes =
     let serialized = JsonSerializer.Serialize recipes
     let bytes = System.Text.Encoding.UTF8.GetBytes serialized
     let memoryStream = toMemoryStream bytes
-    blockBlob.Upload(memoryStream, true)
+    blob.Upload(memoryStream, true)
 
+let getRecipes blobClient () = async {
+    let! recipes = getRecipesDict blobClient
+    return recipeMapToResponseArray recipes
+}
 
-let postRecipe storageConnString (recipe: Recipe) = async {
-    let! recipes = getRecipesDict (storageConnString)
+let postRecipe blobClient (recipe: Recipe) = async {
+    let! recipes = getRecipesDict blobClient
     let withRecipe = Map.add recipe.Name recipe.Ingredients recipes
 
     // TODO: make async and handle errors.
-    uploadRecipes storageConnString withRecipe |> ignore
+    uploadRecipes blobClient withRecipe |> ignore
 
     return recipeMapToResponseArray withRecipe
 }
 
-let deleteRecipe storageConnString (recipeName: string) : Recipe array Async = async {
-    let! recipes = getRecipesDict (storageConnString)
+let deleteRecipe blobClient (recipeName: string) : Recipe array Async = async {
+    let! recipes = getRecipesDict (blobClient)
     let removed = Map.remove recipeName recipes
 
     // TODO: make async and handle errors.
-    uploadRecipes storageConnString removed |> ignore
+    uploadRecipes blobClient removed |> ignore
 
     return recipeMapToResponseArray removed
 }
 
-let dojoApi (config: IConfiguration) = {
-    PostRecipe = postRecipe config.["ConnectionStrings:BlobStorage"]
-    GetRecipes = (fun _ -> getRecipes config.["ConnectionStrings:BlobStorage"])
-    DeleteRecipe = deleteRecipe config.["ConnectionStrings:BlobStorage"]
-}
+let dojoApi (config: IConfiguration) =
+    let blob = config |> Config.FromIConfiguration |> getBlob
+
+    {
+        PostRecipe = postRecipe blob
+        GetRecipes = getRecipes blob
+        DeleteRecipe = deleteRecipe blob
+    }
